92834f27148a45762cb1b3bf12a00f096c816d29
dangnguyen1998 <48938114+dangnguyen1998@users.noreply.github.com>
2019-04-09 08:49:43 +0700

    Add project files.

---
+++ BigSchoolQuiDang/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BigSchoolQuiDang.Models;
+using Microsoft.AspNet.Identity;
+using BigSchoolQuiDang.DTOs;
+
+
+namespace BigSchoolQuiDang.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendace = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendace);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ BigSchoolQuiDang/Controllers/FollowingsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using BigSchoolQuiDang.DTOs;
+using BigSchoolQuiDang.Models;
+using Microsoft.AspNet.Identity;
+namespace BigSchoolQuiDang.Controllers
+{
+    public class FollowingsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public FollowingsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [System.Web.Mvc.HttpPost]
+        public IHttpActionResult Follow(FollowingDto followingDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == followingDto.FolloweeId))
+                return BadRequest("Following already exitst!");
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = followingDto.FolloweeId
+            };
+            _dbContext.Followings.Add(following);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        private IHttpActionResult Ok()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IHttpActionResult BadRequest(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
